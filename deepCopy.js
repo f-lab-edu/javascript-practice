f30a9002d724fcce3f6766b3437992fc13c03b48
sydney-choi <soyeong0730@gmail.com>
2024-01-22 12:39:04 +0900

    feat: deepCopy ts파일 추가

--- deepCopy.js
+++ deepCopy.js
@@ -1,39 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function deepCopy(target) {
-  // 원시 타입, Date 타입인 경우
-  if (!(target instanceof Object) || target instanceof Date) {
-    return target;
-  }
-
-  // Map 타입인 경우
-  if (target instanceof Map) {
-    let mapCopy = new Map();
-    const keys = target.keys();
-    for (let key of keys) {
-      mapCopy.set(key, deepCopy(target.get(key)));
+    // 원시 타입, null인 경우
+    if (!(target instanceof Object) || target === null) {
+        return target;
     }
-
-    return mapCopy;
-  }
-
-  // Set 타입인 경우
-  if (target instanceof Set) {
-    let setCopy = new Set();
-    const keys = target.keys();
-    for (let key of keys) {
-      setCopy.add(deepCopy(key));
+    // Map 타입인 경우
+    if (target instanceof Map) {
+        let mapCopy = new Map();
+        const keys = target.keys();
+        for (let key of keys) {
+            mapCopy.set(key, deepCopy(target.get(key)));
+        }
+        return mapCopy;
     }
-
-    return setCopy;
-  }
-
-  // 참조 타입인 경우
-  let result = Array.isArray(target) ? [] : {};
-  const keys = Object.keys(target);
-  for (let key of keys) {
-    result[key] = deepCopy(target[key]);
-  }
-
-  return result;
+    // Set 타입인 경우
+    if (target instanceof Set) {
+        let setCopy = new Set();
+        const keys = target.keys();
+        for (let key of keys) {
+            setCopy.add(deepCopy(key));
+        }
+        return setCopy;
+    }
+    // 배열 타입인 경우
+    if (Array.isArray(target)) {
+        let arrayCopy = [];
+        for (let t of target) {
+            arrayCopy.push(deepCopy(t));
+        }
+        return arrayCopy;
+    }
+    // 객체 리터럴 타입인 경우
+    let objCopy = target;
+    const keys = Object.keys(objCopy);
+    for (let key of keys) {
+        objCopy[key] = deepCopy(objCopy[key]);
+    }
+    return objCopy;
 }
-
-export default deepCopy;
+exports.default = deepCopy;
---
+++ deepCopy.ts
@@ -0,0 +1,49 @@
+function deepCopy<T>(target: T): T {
+  // 원시 타입, null인 경우
+  if (!(target instanceof Object) || target === null) {
+    return target;
+  }
+
+  // Map 타입인 경우
+  if (target instanceof Map) {
+    let mapCopy = new Map();
+    const keys = target.keys();
+    for (let key of keys) {
+      mapCopy.set(key, deepCopy(target.get(key)));
+    }
+
+    return mapCopy as T;
+  }
+
+  // Set 타입인 경우
+  if (target instanceof Set) {
+    let setCopy = new Set();
+    const keys = target.keys();
+    for (let key of keys) {
+      setCopy.add(deepCopy(key));
+    }
+
+    return setCopy as T;
+  }
+
+  // 배열 타입인 경우
+  if (Array.isArray(target)) {
+    let arrayCopy = [];
+    for (let t of target) {
+      arrayCopy.push(deepCopy(t));
+    }
+
+    return arrayCopy as T;
+  }
+
+  // 객체 리터럴 타입인 경우
+  let objCopy = target as {[key: string]: any};
+  const keys = Object.keys(objCopy);
+  for (let key of keys) {
+    objCopy[key] = deepCopy(objCopy[key]);
+  }
+
+  return objCopy as T;
+}
+
+export default deepCopy;
