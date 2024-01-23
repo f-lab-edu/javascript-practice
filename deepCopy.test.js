47a0d20f89d95da6e04c42a01a10c63d82ec5f00
sydney-choi <soyeong0730@gmail.com>
2024-01-24 00:58:31 +0900

    fix: test code수정

--- deepCopy.test.js
+++ deepCopy.test.js
@@ -1,28 +1,77 @@
 import deepCopy from "./deepCopy";
 
-test("deepCopy test", () => {
-  const map = new Map();
-  map.set("test", 12345);
-  map.set("test2", { a: 123, b: 456 });
-  map.set("test3", [1, 2, 3, 4, 5]);
+describe('deepCopy test', () => {
+  test("number copy", () => {
+    expect(1).toBe(1);
+  });
 
-  const set = new Set();
-  set.add(12345);
-  set.add({ a: 123, b: 456 });
-  set.add([1, 2, 3, 4, 5]);
+  test("string copy", () => {
+    expect(deepCopy('abcde')).toBe('abcde');
+  });
 
-  const target = {
-    number: 12345,
-    string: "abcde",
-    boolean: false,
-    null: null,
-    undefined: undefined,
-    symbol: Symbol("symbol"),
-    object: { a: 123, b: 456 },
-    date: new Date(),
-    map,
-    set,
-  };
+  test("boolean copy", () => {
+    expect(deepCopy(false)).toBe(false);
+  });
 
-  expect(deepCopy(target)).toEqual(target);
+  test("null copy", () => {
+    expect(deepCopy(null)).toBe(null);
+  });
+
+  test("undefined copy", () => {
+    expect(deepCopy(undefined)).toBe(undefined);
+  });
+
+  test("Symbol copy", () => {
+    const symbol = Symbol("symbol");
+    expect(deepCopy(symbol)).toBe(symbol);
+  });
+
+  test("Date copy", () => {
+    const date = new Date();
+    expect(deepCopy(date)).toBe(date);
+  });
+
+  test("Map copy", () => {
+    const map = new Map();
+    map.set("test", 12345);
+    map.set("test2", { a: 123, b: 456 });
+    map.set("test3", [1, 2, 3, 4, 5]);
+
+    const mapCopy = deepCopy(map);
+    map.set("test3", [6, 2, 3, 4, 5]);
+    expect(mapCopy.get("test3")).toBe([1, 2, 3, 4, 5]);
+  });
+
+  test("Set copy", () => {
+    const set = new Set();
+    set.add(12345);
+    set.add({ a: 123, b: 456 });
+    set.add([1, 2, 3, 4, 5]);
+
+    const setCopy = deepCopy(set);
+    set.delete([1, 2, 3, 4, 5]);
+
+    expect(setCopy.has([1, 2, 3, 4, 5])).toBe(true);
+  });
+
+  test("array copy", () => {
+    const array = [1, 2, 3, 4, 5];
+    const arrayCopy = deepCopy(array);
+    array[0] = 3;
+    expect(arrayCopy[0]).toBe(1);
+  });
+
+  test("object copy", () => {
+    const obj = {
+      a: 1,
+      b:2,
+      c: 3,
+      d: {
+        e: 1
+      }
+    }
+    const objCopy = deepCopy(obj);
+    obj.d.e = 4;
+    expect(objCopy.d.e).toBe(1);
+  });
 });
